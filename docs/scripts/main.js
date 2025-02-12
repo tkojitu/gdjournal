b42ffb3a81b73de9243c5ca378b8228a8d09bedc
gdgdiler <gdgdiler@gmail.com>
2025-02-12 13:20:28 +0900

    indentation

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,22 +1,22 @@
 function getTimestamp() {
-    let iso = new Date().toISOString();
-    let str = iso.replace(/[T:-]/g, "");
-    return str.replace(/\..*/, "");
+  let iso = new Date().toISOString();
+  let str = iso.replace(/[T:-]/g, "");
+  return str.replace(/\..*/, "");
 }
 
 function gdjournal() {
-    const text = document.getElementById("textarea").value;
-    const blob = new Blob([text], {type: "text/plain"});
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = getTimestamp() + ".txt";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(link.href);
+  const text = document.getElementById("textarea").value;
+  const blob = new Blob([text], {type: "text/plain"});
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = getTimestamp() + ".txt";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  URL.revokeObjectURL(link.href);
 }
 
 function clearTextarea() {
-	let text = document.getElementById("textarea");
-	text.value = "";
+  let text = document.getElementById("textarea");
+  text.value = "";
 }
